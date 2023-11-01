ef20beaff5acf33b3e6d911ad8b3232517f16b75
kumibrr <isra.lora99@gmail.com>
2023-11-01 12:00:27 +0100

    fix(e2e): removed types for cypress and testing library as they're too new for the current nod and typescript version

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+// /// <reference types="cypress" />
 import "@testing-library/cypress/add-commands";
 // ***********************************************
 // This example commands.ts shows you how to
