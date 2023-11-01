8a5bfcd289bf171901f6702e5e44a634df22d535
kumibrr <isra.lora99@gmail.com>
2023-11-01 16:09:12 +0100

    feat(products): implemented list of products

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductsService } from "./data-access/products.service";
+import { Observable } from "rxjs";
+import { Product } from "./model/product";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent implements OnInit {
+  products$: Observable<Product[]>;
 
-  constructor() { }
+  constructor(private products: ProductsService) {}
 
   ngOnInit() {
+    this.products$ = this.products.getProducts();
   }
-
 }
