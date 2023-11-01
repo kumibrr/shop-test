0d681db096ec2c337ca07c333cec82609adcbe5f
kumibrr <isra.lora99@gmail.com>
2023-11-01 19:33:45 +0100

    feat(cart): included Omit type from ts 3.5

--- src/app/shared/feature/cart/data-access/cart.service.ts
+++ src/app/shared/feature/cart/data-access/cart.service.ts
@@ -4,16 +4,23 @@ import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Cart } from "../model/cart";
 import { map } from "rxjs/operators";
+import { Omit } from "src/app/shared/utils/omit";
 
+type CartResponse = Omit<Cart, "products"> & {
+  products: Array<{
+    productId: number;
+    quantity: number;
+  }>;
+};
 @Injectable({
   providedIn: "root",
 })
 export class CartService {
   constructor(private http: HttpClient) {}
 
-  getCartByUserId(id: number): Observable<Cart> {
+  getCartByUserId(id: number): Observable<CartResponse> {
     return this.http
-      .get<Cart[]>(`${environment.apiUrl}/carts/user/${id}`)
+      .get<CartResponse[]>(`${environment.apiUrl}/carts/user/${id}`)
       .pipe(map((carts) => carts[0]));
   }
 }
