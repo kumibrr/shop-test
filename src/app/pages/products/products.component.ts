2cab207ffb687fd3caa7eb76c0d7b8d0f8b206b8
Israel de la Barrera <isra.lora99@gmail.com>
2023-11-01 17:46:46 +0100

    Feature/products (#4)
    Created a product list page that shows products to the authenticated
    user.

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { ProductsService } from "./data-access/products.service";
+import { Observable } from "rxjs";
+import { Product } from "./model/product";
+
+@Component({
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"],
+})
+export class ProductsComponent implements OnInit {
+  products$: Observable<Product[]>;
+
+  constructor(private products: ProductsService) {}
+
+  ngOnInit() {
+    this.products$ = this.products.getProducts();
+  }
+
+  itemSelected(id: number) {
+    console.log(id);
+  }
+}
---
+++ src/app/pages/products/products.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProductsComponent } from "./products.component";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: "",
+        component: ProductsComponent,
+      },
+    ]),
+  ],
+  declarations: [ProductsComponent],
+})
+export class ProductsModule {}
