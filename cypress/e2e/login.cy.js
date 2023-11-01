9393c96b136d87e1d3d4c6063602fcef46c951f3
kumibrr <isra.lora99@gmail.com>
2023-11-01 12:09:46 +0100

    feat(login/e2e): created initial login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("login", () => {
+  it("login button should be disabled when invalid", () => {
+    cy.visit("http://localhost:4200/login");
+
+    cy.findByText("Log in").should("be.disabled");
+
+    cy.findByText("Username").click().type("username");
+    cy.findByText("Log in").should("be.disabled");
+    cy.findByText("Username").clear();
+
+    cy.findByText("Password").click().type("password");
+    cy.findByText("Log in").should("be.disabled");
+  });
+
+  it("login button should be enabled when valid", () => {
+    cy.visit("http://localhost:4200/login");
+
+    cy.findByText("Username").click().type("username");
+    cy.findByText("Password").click().type("password");
+    cy.findByText("Log in").should("not.be.disabled");
+  });
+});
