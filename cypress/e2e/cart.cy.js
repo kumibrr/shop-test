6d33d29b71d7374162f062ff06a795ce9e1d42fe
kumibrr <isra.lora99@gmail.com>
2023-11-03 17:23:14 +0100

    fix(products/e2e): fixed e2e tests being broken after imlementing shopping cart

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -2,19 +2,22 @@ describe("Cart", () => {
   it("loads cart data", () => {
     cy.standardLogin();
     cy.intercept("https://fakestoreapi.com/carts/user/2").as("getCart");
-    cy.visit("http://localhost:4200/cart/2");
+    cy.visit("http://localhost:4200/");
     cy.wait("@getCart");
-    cy.findByText(
-      /Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i
-    ).should("exist");
+    cy.findByText("shopping cart").click();
+    cy.get("#shopping-cart")
+      .findByText(/Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i)
+      .should("exist");
   });
 
   it("should display the correct price", () => {
     cy.standardLogin();
     cy.intercept("https://fakestoreapi.com/carts/user/2").as("getCart");
-    cy.visit("http://localhost:4200/cart/2");
+    cy.visit("http://localhost:4200/");
     cy.wait("@getCart");
-    cy.findByText(/Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i)
+    cy.findByText("shopping cart").click();
+    cy.get("#shopping-cart")
+      .findByText(/Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i)
       .parent()
       .as("row");
 
@@ -27,25 +30,29 @@ describe("Cart", () => {
     cy.standardLogin();
     cy.intercept("https://fakestoreapi.com/carts/user/2").as("getCart");
     cy.intercept("https://fakestoreapi.com/carts/3").as("removeItem");
-    cy.visit("http://localhost:4200/cart/2");
+    cy.visit("http://localhost:4200/");
     cy.wait("@getCart");
-    cy.findByText(/Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i)
+    cy.findByText("shopping cart").click();
+    cy.get("#shopping-cart")
+      .findByText(/Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i)
       .parent()
       .as("row");
-    cy.get("@row").findByLabelText("remove item").click();
+    cy.get("@row").findByLabelText("Remove item").click();
     cy.wait("@removeItem");
-    cy.findByText(
-      "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
-    ).should("not.exist");
+    cy.get("#shopping-cart")
+      .findByText("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops")
+      .should("not.exist");
   });
 
   it("should update quantity and update price", () => {
     cy.standardLogin();
     cy.intercept("https://fakestoreapi.com/carts/user/2").as("getCart");
     cy.intercept("https://fakestoreapi.com/carts/3").as("updateCart");
-    cy.visit("http://localhost:4200/cart/2");
+    cy.visit("http://localhost:4200/");
     cy.wait("@getCart");
-    cy.findByText(/Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i)
+    cy.findByText("shopping cart").click();
+    cy.get("#shopping-cart")
+      .findByText(/Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i)
       .parent()
       .as("row");
     cy.get("@row").findByDisplayValue("2").type("{backspace}3{enter}");
